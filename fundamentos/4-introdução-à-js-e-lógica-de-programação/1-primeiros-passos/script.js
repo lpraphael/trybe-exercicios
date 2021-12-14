631e28b1461a9cc82f515a8381ce64de0b259020
Raphael Perazzo <raphael.perazzo@gmail.com>
2021-12-14 19:22:27 -0300

    concluindo questao 5

--- fundamentos/4-introdução-à-js-e-lógica-de-programação/1-primeiros-passos/script.js
+++ fundamentos/4-introdução-à-js-e-lógica-de-programação/1-primeiros-passos/script.js
@@ -1,6 +1,8 @@
 const a = 5;
 const b = 10;
+
 // questão 1
+
 function adicao (a, b) {
   let resultado = 0;
   resultado = a + b;
@@ -46,7 +48,6 @@ const x = 110;
 const y = 110;
 const z = 110;
 
-
 function maiorDeTres (x, y, z) {
   let resultado;
   if (x < z && y < z) {
@@ -62,8 +63,52 @@ function maiorDeTres (x, y, z) {
   return resultado;
 }
 
-// questão 3
-const numero = 10;
+// questão 4
 
-function positivoNegativo ()
+const numero = 0;
 
+function positivoNegativo (numero) {
+  let resultado;
+  if (numero % 2 === 0 && numero !== 0){
+    resultado = 'positive'
+  } else if ( numero % 2 !== 0) {
+    resultado = 'negative';
+  } else if (numero === 0){
+    resultado = 'zero';
+  }
+  return resultado;
+}
+
+// questão 5
+
+const angulo1 = 30;
+const angulo2 = 60;
+const angulo3 = 90;
+
+function triangulo (angulo1, angulo2, angulo3){
+  let resultado;
+  if (Math.sign(angulo1) === -1 || Math.sign(angulo2) === -1 || Math.sign(angulo3) === -1) {
+    resultado = 'erro'
+    return resultado;
+  }
+  if (angulo1 + angulo2 + angulo3 === 180) {
+    resultado = true;
+  } else if (angulo1 + angulo2 + angulo3 !== 180){
+    resultado = false;
+  }
+  return resultado;
+}
+
+// questão 6
+
+
+
+// questão 7
+
+// questão 8
+
+// questão 9
+
+// questão 10 
+
+// questão 11
