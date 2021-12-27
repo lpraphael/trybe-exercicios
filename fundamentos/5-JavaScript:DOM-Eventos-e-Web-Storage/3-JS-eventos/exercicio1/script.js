a9d9539cf9ac621607034131fc0b0c820482f325
Raphael Lyra Perazzo Leite Galvão <raphael.perazzo@gmail.com>
2021-12-27 16:54:20 -0300

    concluido exercicio2

--- fundamentos/5-JavaScript:DOM-Eventos-e-Web-Storage/3-JS-eventos/exercicio1/script.js
+++ fundamentos/5-JavaScript:DOM-Eventos-e-Web-Storage/3-JS-eventos/exercicio1/script.js
@@ -8,19 +8,49 @@ const myWebpage = document.getElementById('my-spotrybefy');
 // 1. Copie esse arquivo e edite apenas ele;
 // 1.1. Antes de começar os exercícios, use o LiveServer para dar uma olhada em como está a página no navegador.
 // 1.2. Note que uma das caixas está um pouco acima das outras. Por que isso ocorre?
+// R - por causa da funcao translateY, que reposiciona o elemento de acordo com o parametro passado
 
 // 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
 
+function classTech(event){
+  
+  const tech = document.querySelector('.tech');
+  tech.classList.remove('tech');
+  event.target.classList.add('tech');
+  input.value = '';
+}
+
+firstLi.addEventListener('click', classTech);
+secondLi.addEventListener('click', classTech);
+thirdLi.addEventListener('click', classTech);
+
+
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
+
+  input.addEventListener('input', function(event) {
+    const techElement = document.querySelector('.tech');
+    techElement.innerText = event.target.value;
+  })
+
 
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portifólio?
+myWebpage.addEventListener('dblclick', function(event){
+  window.location.replace('https://www.github.com/LPRaphael/');
+});
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
+myWebpage.addEventListener('mouseover', function (event){
+  event.target.style.color = '#' + Math.floor(Math.random() * 16777215);
+});
+
+myWebpage.addEventListener ('mouseout', function(event){
+  event.target.style.color = 'unset';
+});
 
 // Segue abaixo um exemplo do uso de event.target:
 
