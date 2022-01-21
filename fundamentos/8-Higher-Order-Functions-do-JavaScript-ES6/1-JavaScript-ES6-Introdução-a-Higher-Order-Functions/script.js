5719830884f1948de1c4a945d6db21fbac22d82d
Raphael Lyra Perazzo Leite Galvão <raphael.perazzo@gmail.com>
2022-01-21 15:36:43 -0300

    exercicio 8.1 parte 1 concluido

---
+++ fundamentos/8-Higher-Order-Functions-do-JavaScript-ES6/1-JavaScript-ES6-Introdução-a-Higher-Order-Functions/script.js
@@ -0,0 +1,9 @@
+const acordando = () => 'Acordando!!';
+const cafe = () => 'Bora tomar cafe!!';
+const dormir = () => 'Partiu dormir!!'
+
+const doingThings = (parameter) => {
+  console.log(parameter());
+}
+
+doingThings(acordando);
