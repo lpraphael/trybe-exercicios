f604609474cb3882ab1b4b5a6e2507b512233b32
Raphael Lyra Perazzo Leite Galvão <raphael.perazzo@gmail.com>
2022-02-22 09:32:38 -0300

    atualizando logica do exercicio 1

--- front-end/bloco-11/componentes-com-Estados-Eventos-e-Forumlários-com-React/exercicio-1/src/App.js
+++ front-end/bloco-11/componentes-com-Estados-Eventos-e-Forumlários-com-React/exercicio-1/src/App.js
@@ -1,25 +1,37 @@
 import React from 'react';
 import './App.css';
 
-function handleClick1() {
-  console.log('clicou no botão 1!')
-}
-
-function handleClick2() {
-  console.log('clicou no botão 2!')
-}
-
-function handleClick3() {
-  console.log('clicou no botão 3!')
-}
 
 class App extends React.Component {
-  render() {
+  constructor () {
+    super ()
+
+    this.handleClick1 = this.handleClick1.bind(this)
+    this.handleClick2 = this.handleClick2.bind(this)
+    this.handleClick3 = this.handleClick3.bind(this)
+
+    console.log('componente sendo construido');
+
+  }
+  handleClick1() {
+    console.log(this)
+    console.log('clicou no botão 1!')
+  }
+    
+    handleClick2() {
+      console.log('clicou no botão 2!')
+    }
+    
+    handleClick3() {
+      console.log('clicou no botão 3!')
+    }
+    render() {
+    console.log(this);
     return (
       <div className="App">
-        <button onClick={handleClick1} >botão 1</button>
-        <button onClick={handleClick2} >botão 2</button>
-        <button onClick={handleClick3} >botão 3</button>
+        <button onClick={this.handleClick1} >botão 1</button>
+        <button onClick={this.handleClick2} >botão 2</button>
+        <button onClick={this.handleClick3} >botão 3</button>
       </div>
     );
   }
