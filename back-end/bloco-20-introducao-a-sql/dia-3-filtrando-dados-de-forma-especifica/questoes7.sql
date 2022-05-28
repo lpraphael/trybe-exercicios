188021523519898fd1f7b56e375937b4c670510b
Raphael Lyra Perazzo Leite Galvão <raphael.perazzo@gmail.com>
2022-05-28 20:26:49 -0300

    questoes 4 a 7 concluidas

--- back-end/bloco-20-introducao-a-sql/dia-3-filtrando-dados-de-forma-especifica/questoes7.sql
+++ back-end/bloco-20-introducao-a-sql/dia-3-filtrando-dados-de-forma-especifica/questoes7.sql
@@ -1,1 +1,4 @@
 -- Ufa! Por fim, faça uma query para exibir o número de vendas feitas entre o dia 15/04/2018 e o dia 30/07/2019.
+SELECT *
+FROM PecasFornecedores.Vendas
+WHERE order_date BETWEEN '2018-04-15' AND '2019-07-30';
