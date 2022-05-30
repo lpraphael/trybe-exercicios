2441792ca4139cb3aa25f440aa1930c326ca53a8
Raphael Lyra Perazzo Leite Galvão <raphael.perazzo@gmail.com>
2022-05-29 23:04:17 -0300

    questoes 7 a 12 concluidas

--- back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/questoes12.sql
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/questoes12.sql
@@ -1,1 +1,3 @@
 -- Escreva uma query que só exiba as informações dos funcionários cujo o primeiro nome tenha oito ou mais caracteres.
+SELECT * FROM hr.employees
+WHERE length(FIRST_NAME) >= 8;
--- back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/questoes7.sql
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/questoes7.sql
@@ -1,1 +1,3 @@
 -- Escreva uma query que exiba a quantidade de dinheiro necessária para efetuar o pagamento de cada profissão (JOB_ID).
+SELECT JOB_ID, COUNT(*), SUM(SALARY) FROM hr.employees
+GROUP BY JOB_ID;
