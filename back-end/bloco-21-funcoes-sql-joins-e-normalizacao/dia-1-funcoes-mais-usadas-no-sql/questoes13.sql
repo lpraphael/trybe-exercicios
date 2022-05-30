f2270778e7a10aea21d2a5c26723e923da80e44e
Raphael Lyra Perazzo Leite Galvão <raphael.perazzo@gmail.com>
2022-05-29 23:41:22 -0300

    questoes 13 a 18 concluidas

--- back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/questoes13.sql
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/questoes13.sql
@@ -1,1 +1,2 @@
 -- Escreva uma query que exiba as seguintes informações de cada funcionário: id, primeiro nome e ano no qual foi contratado (exiba somente o ano).
+SELECT FIRST_NAME, YEAR(HIRE_DATE) FROM hr.employees;
--- back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/questoes17.sql
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/questoes17.sql
@@ -1,1 +1,3 @@
 -- Escreva uma query que exiba o sobrenome e a data de contratação de todos os funcionário contratados em julho de 1987.
+SELECT LAST_NAME, HIRE_DATE FROM hr.employees
+WHERE YEAR(HIRE_DATE) = 1987; 
