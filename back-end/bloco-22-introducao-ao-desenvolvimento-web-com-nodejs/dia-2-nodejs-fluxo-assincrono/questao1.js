cdcc327313239d7c9773e2f0d917c05aff97fda1
Raphael Lyra Perazzo Leite Galvão <raphael.perazzo@gmail.com>
2022-06-14 11:42:25 -0300

    questao 1 concluida

---
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/questao1.js
@@ -0,0 +1,21 @@
+const readLine = require('readline-sync')
+
+function questao1(num1, num2, num3) {
+
+const promise = new Promise((resolve, reject) => {
+if(typeof num1 !== 'number' || typeof num2 !== 'number' || typeof num3 !== 'number') {
+reject(new Error('Informe apenas números'))
+}
+const result = (num1 + num2) * num3;
+if(result < 50) {
+reject(new Error('Valor muito baixo'))
+}
+resolve(result)
+});
+return promise
+}
+
+questao1(4, 5, 6)
+.then(result => console.log(`sucesso: ${result}`))
+.catch(err => console.log(`erro: ${err.message}`));
+
