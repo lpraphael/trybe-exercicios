520117774921ee5bca9f652ed956ca4c5e16b436
lpraphael <raphael.perazzo@gmail.com>
2022-10-21 23:42:50 -0300

    resolvendo questoes 4 e 5 do exercicio

--- Ciência_da_Computação/1.introdução_a_python/dia_01-Aprendendo_Python/exercicio-1/exercicio1.py
+++ Ciência_da_Computação/1.introdução_a_python/dia_01-Aprendendo_Python/exercicio-1/exercicio1.py
@@ -16,3 +16,22 @@ def make_square(n):
     """imprime na tela um quadrado feito de * com lado e altura de tamanho n"""
     for row in range(n):
         print(n * "*")
+
+
+def find_biggest_name(names):
+    """função recebe uma lista de nomes e retorna o com mais caracteres"""
+    biggest_name = names[0]
+    for name in names:
+        if len(name) > len(biggest_name):
+            biggest_name = name
+    return biggest_name
+
+
+def paint_cost(area):
+    """calcula quantidade de tinta necessárias pra pintar parede e o custo"""
+    can_price = 80
+    required_liters = area / 3
+    required_cans = required_liters // 18
+    if required_liters % 18:
+        required_cans += 1
+    return required_cans, required_cans * can_price
